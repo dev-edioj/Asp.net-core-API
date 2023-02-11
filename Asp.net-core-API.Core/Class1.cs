6c21d4962cb89255f0b5e15c8c13bf87cbbd6565
Edio <junioedio.r@gmail.com>
2023-02-11 13:51:45 -0300

    V2 - Core

---
+++ Asp.net-core-API.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Asp.net_core_API.Core
+{
+    public class Class1
+    {
+
+    }
+}
--- Asp.net-core-API.Core/obj/Debug/net6.0/Asp.net-core-API.Core.AssemblyInfo.cs
+++ Asp.net-core-API.Core/obj/Debug/net6.0/Asp.net-core-API.Core.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Asp.net-core-API")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Asp.net-core-API.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Asp.net-core-API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Asp.net-core-API")]
+[assembly: System.Reflection.AssemblyProductAttribute("Asp.net-core-API.Core")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Asp.net-core-API.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
